9587e7d6d50e33d766bc3fa34626c75c2fbaba7f
pandreyanov <pandreyanov@gmail.com>
2022-05-03 12:01:55 +0300

    commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
-setuptools.setup(name='pashas_fpa',
+setuptools.setup(name='simple_fpa',
 version='0.1',
-description='A package for the "Nonparametric inference on counterfactuals in sealed first-price auctions" paper.
-',
+description='A package for the "Nonparametric inference on counterfactuals in sealed first-price auctions" paper.',
 author='Pasha Andreyanov',
 author_email='pandreyanov@gmail.com',
+install_requires=['numpy','pandas','scipy'],
 packages=setuptools.find_packages(),
 zip_safe=False)
